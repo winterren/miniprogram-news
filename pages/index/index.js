fbdb1ac775af31b72df9dcf4d89874c0196ffa5b
winterren <winterren@msn.com>
2018-11-21 16:48:43 +0800

    refresh index

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,65 +1,62 @@
-// pages/index/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    articlesResult:[],
+    catagory:['gn', 'gj', 'cj', 'yl', 'js', 'ty', 'other'],
+    catagoryZh:{
+      'gn': '国内',
+      'gj': '国际',
+      'cj': '财经',
+      'yl': '娱乐',
+      'js': '军事',
+      'ty': '体育',
+      'other': '其他'
+    },
+    catagoryActive:'gn',
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    this.getNews();
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
+  getNews(){
+  
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
+      data: {
+        type: 'gn'
+      },
+      success: res => {
+        let result = res.data.result;
+        this.setData({
+          articlesResult: result,
+        });
+        console.log(result);
+        for(let i=0;i<result.length;i++){
+          console.log(this.convertDate(result[i].date));
+        }
+      },
+    })
+  },
+  convertDate(dateString){
+    let stringDay = dateString.substr(5, 2).replace(/\b(0+)/gi, "") +"月"+ dateString.substr(8, 2).replace(/\b(0+)/gi, "")+"日";
+    let stringTime = dateString(11,5);
+    return stringDay + stringTime;
+  },
   onReady: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
   onHide: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload: function () {
-
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function () {
 
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
 
   },
-
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage: function () {
 
   }
