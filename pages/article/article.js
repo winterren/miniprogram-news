f7b8201e47cfe07d58729769234d05c4384cc45d
winterren <winterren@msn.com>
2018-11-28 05:45:07 +0800

    finish basic pages

--- pages/article/article.js
+++ pages/article/article.js
@@ -1,65 +1,40 @@
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    
+  data:{
+    articleId: 1523074607672,
+    articleTitle:"",
+    articleDate:"",
+    tempContent:"<h1>hello</h1>",
+    articleContent:[],
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
+  onLoad(option){
+    // 读取navigateto传入的id
+    let id = option.id;
+    this.setData({
+      articleId: option.id,
+    });
+    // 获得该id的文章内容
+    this.getArticle();
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  getArticle(){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/detail',
+      data: {
+        id: this.data.articleId,
+      },
+      success: res => {
+        let result = res.data.result;
+        let timeDate = new Date(result.date);
+        let timeString = timeDate.getFullYear() + '年' 
+          + (timeDate.getMonth()+1)+ '月' 
+          + (timeDate.getDay()+1)+'日' 
+          + timeDate.getHours()+ '时' 
+          + timeDate.getMinutes()+'分';
+        this.setData({
+          articleTitle: result.title,
+          articleDate: timeString,
+          articleContent: result.content,
+        })
+      },
+    })
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,10 @@
 Page({
   data: {
-    articlesResult:[],
-    articlesResultFormat:[],
-    catagory:['gn', 'gj', 'cj', 'yl', 'js', 'ty', 'other'],
-    catagoryZh:{
+    articlesResultFormat: [], //ajax返回文章列表
+    articlesResultFormatFrom1: [], //ajax返回文章列表（首图文章）
+    articlesResultFormatFrom2: [], //ajax返回文章列表（除了首图的文章）
+    catagory:['gn', 'gj', 'cj', 'yl', 'js', 'ty', 'other'], //新闻分类
+    catagoryZh:{ //新闻分类（对应中文）
       'gn': '国内',
       'gj': '国际',
       'cj': '财经',
@@ -12,62 +13,65 @@ Page({
       'ty': '体育',
       'other': '其他'
     },
-    catagoryActive:'gn',
+    catagoryActive:'gn', //默认分类
   },
-  onLoad: function (options) {
+  onLoad () {
     this.getNews();
   },
-  getNews(){
-  
+  // 获取文章列表函数：请求ajax
+  getNews () {
     wx.request({
       url: 'https://test-miniprogram.com/api/news/list',
       data: {
-        type: 'gn'
+        type: this.data.catagoryActive,
       },
       success: res => {
         let result = res.data.result;
         this.setData({
           articlesResult: result,
         });
-        console.log(result);
-        console.log(typeof(result[1].date));
-        for(let i=0;i<result.length;i++){
-          // console.log(this.convertDate(result[i].date));
-        };
+        this.convertFormat(result);
       },
     })
   },
-  // convertDate(res){
-  //   let stringDay;
-  //   let stringTime;
-  //   for (let i = 0; i < res.length; i++){
-  //     let stringDay = dateString.substr(5, 2).replace(/\b(0+)/gi, "") + "月" + dateString.substr(8, 2).replace(/\b(0+)/gi, "") + "日";
-  //     let stringTime = dateString(11, 5);
-  //     this.setData({
-  //       articlesResultFormat:
-  //     })
-  //   }
-    
-  //   return stringDay + stringTime;
-  // },
-  onReady: function () {
-
+  // 转换格式函数：将ajax的文章列表重新整理格式后存储到articlesResultFormat等中
+  convertFormat(res){
+    let tempArticle=[];
+    let stringDay;
+    let stringTime;
+    let stringSource;
+    for (let i = 0; i < res.length; i++){
+      stringDay = res[i].date.substr(5, 2).replace(/\b(0+)/gi, "") + "月" + res[i].date.substr(8, 2).replace(/\b(0+)/gi, "") + "日";
+      stringTime = res[i].date.substr(11, 5);
+      stringSource = res[i].source ? res[i].source : '未知来源';
+      tempArticle.push({
+        'id': res[i].id,
+        'title': res[i].title,
+        'firstImage': res[i].firstImage,
+        'date':(stringDay+' '+stringTime),
+        'source': stringSource,
+      });
+    };
+    this.setData({
+      articlesResultFormat: tempArticle,
+      articlesResultFormatFrom1: tempArticle.slice(0, 1),
+      articlesResultFormatFrom2: tempArticle.slice(1), 
+    });
   },
-  onShow: function () {
-
+  // 点击切换分类函数
+  onTapCatagory(res){
+    let index = res.currentTarget.dataset.index;
+    this.setData({
+      catagoryActive: this.data.catagory[index],
+    });
+    this.getNews();
   },
-  onHide: function () {
-
+  // 点击跳转文章函数
+  onTapArticle(res){
+    let id = res.currentTarget.dataset.id;
+    console.log(id);
+    wx.navigateTo({
+      url: '../article/article?id='+id,
+    })
   },
-  onUnload: function () {
-  },
-  onPullDownRefresh: function () {
-
-  },
-  onReachBottom: function () {
-
-  },
-  onShareAppMessage: function () {
-
-  }
 })
